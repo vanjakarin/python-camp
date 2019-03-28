827d57a5f58e0ca109468a234dc015a1348b127b
Vanja Tufvesson <vanjakarin@gmail.com>
2019-03-28 20:13:33 +0100

    Commiting Pias changes on vanjas comp

---
+++ pias projekt.py
@@ -0,0 +1,24 @@
+name = "Pia"
+print("Hello " + name)
+
+if 5>2:
+    print("Five is greater than two")
+
+raining = 6
+
+if raining >= 6:
+    print("it is raining")
+elif raining == 6:
+    print("det regnar lite")
+else:
+    print("have a nice day")
+
+energy = int(input("how much energy?"))
+
+if energy <= 4:
+    print ("Time for a snack")
+if energy > 4 and energy < 7:
+    print ("You´re doing great")
+if energy > 7:
+    print ("woha time to slow down")
+
